694b7c579a452dbf2b7a8d565c47456c06612c34
Kishor Mahmud <kishor.betopia@gmail.com>
2025-10-18 14:39:59 +0600

    pusher integrate

--- src/config/pusher.js
+++ src/config/pusher.js
@@ -3,6 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// ✅ Debugging: check environment variables
+console.log("PUSHER_APP_ID:", process.env.PUSHER_APP_ID);
+console.log("PUSHER_KEY:", process.env.PUSHER_KEY);
+console.log("PUSHER_SECRET:", process.env.PUSHER_SECRET);
+console.log("PUSHER_CLUSTER:", process.env.PUSHER_CLUSTER);
+console.log("PUSHER_USE_TLS:", process.env.PUSHER_USE_TLS);
+
 const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_KEY,
